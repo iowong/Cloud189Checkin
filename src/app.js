c8df2d81ec08d1be03f99899d990bf441a80ba7a
iowong <74191259+iowong@users.noreply.github.com>
2023-06-06 15:04:07 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -216,7 +216,7 @@ const pushServerChan = (title, desp) => {
     title,
     desp,
   };
-superagent.get("https://api.day.app/wXt6nakVkRuXeRsJYzvjkH/".desp)
+superagent.get("https://api.day.app/wXt6nakVkRuXeRsJYzvjkH/".content)
     .end( (err,res)=> {
         if (err) {
             console.log("出错：" + err);
